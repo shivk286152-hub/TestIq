45e67b70ff52dad28af4e15b0b3ebb49cc6e3d2c
shivk286152-hub <shivk286152@gmail.com>
2026-02-24 21:12:37 +0530

    Update math_filters.py

--- User/templatetags/math_filters.py
+++ User/templatetags/math_filters.py
@@ -1,1 +1,23 @@
 
+# profile/templatetags/math_filters.py
+from django import template
+
+register = template.Library()
+
+@register.filter
+def div(value, arg):
+    """Divide the value by the argument"""
+    try:
+        if float(arg) != 0:
+            return float(value) / float(arg)
+        return 0
+    except (ValueError, TypeError):
+        return 0
+
+@register.filter
+def mul(value, arg):
+    """Multiply the value by the argument"""
+    try:
+        return float(value) * float(arg)
+    except (ValueError, TypeError):
+        return 0
