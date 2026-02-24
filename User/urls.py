ba0d5bb0fafce36de4d3c42360d6a78daf09acb6
shivk286152-hub <shivk286152@gmail.com>
2026-02-24 22:46:38 +0530

    Update urls.py

--- User/urls.py
+++ User/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path
+urlpatternsr' django.urls import path
 from .views import profile_view
-app_name = 'User'
+
 
 urlpatterns = [
     path('', profile_view, name='profile'),
