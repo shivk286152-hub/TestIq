4fba5b3fd5dac7904bf86b6c4d4cf3a9b58ca2f7
shivk286152-hub <shivk286152@gmail.com>
2026-03-12 13:44:40 +0530

    Update 0003_sync_db_state.py

--- exams/migrations/0003_sync_db_state.py
+++ exams/migrations/0003_sync_db_state.py
@@ -3,6 +3,18 @@
 from django.db import migrations
 
 
+def fix_duplicate_options(apps, schema_editor):
+    Option = apps.get_model('exams', 'Option')
+    
+    # Find duplicates for question_id=1 with order=0
+    duplicates = Option.objects.filter(question_id=1, order=0)
+    
+    if duplicates.count() > 1:
+        # Keep the first one, delete others
+        first = duplicates.first()
+        duplicates.exclude(id=first.id).delete()
+
+
 class Migration(migrations.Migration):
 
     dependencies = [
@@ -10,4 +22,5 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.RunPython(fix_duplicate_options),
     ]
