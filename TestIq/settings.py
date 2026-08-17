07347aa35d7a8d9b0c543ccd7cf5758600b12894
mehradharmi <dharmendramehra15@gmail.com>
2026-08-17 12:36:51 +0530

    Initial commit

--- TestIq/settings.py
+++ TestIq/settings.py
@@ -110,11 +110,17 @@ TEMPLATES = [
 # ----------------------------
 # DATABASE
 # ----------------------------
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL")
+    )
 }
 
 # ----------------------------
